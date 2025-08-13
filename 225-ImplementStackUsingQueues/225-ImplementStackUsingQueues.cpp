2aa1bd05cf5b685be4f19a2ad0fd04241087bb3e
RahulWagh619 <rahulwagh196122@gmail.com>
2025-08-13 15:36:03 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.cpp
@@ -0,0 +1,53 @@
+// Last updated: 8/13/2025, 3:36:03 PM
+class MyStack {
+public:
+    queue<int>q1;
+    queue<int>q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        if(q1.empty()){
+         q1.push(x);
+        }
+        else{
+            q2.push(x);
+            while(!q1.empty()){
+                int temp=q1.front();
+                q1.pop();
+                q2.push(temp);
+            }
+           while(!q2.empty()){
+                int temp=q2.front();
+                q2.pop();
+                q1.push(temp);
+            }
+
+        }
+    }
+    
+    int pop() {
+        int val=q1.front();
+        q1.pop();
+        return val;
+
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
