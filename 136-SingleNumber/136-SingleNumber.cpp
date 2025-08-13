5755f7c2a5f662a2749cfb8b86840be0ae2b22c1
RahulWagh619 <rahulwagh196122@gmail.com>
2025-08-13 15:36:31 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/13/2025, 3:36:31 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& a) {
+        unordered_map<int,int>mpp;
+        for(int i=0;i<a.size();i++){
+            mpp[a[i]]++;
+        }
+        int store;
+        for(int i=0;i<a.size();i++){
+            if(mpp[a[i]]==1){
+                store=a[i];
+            }
+        }
+        return store;
+    }
+};
