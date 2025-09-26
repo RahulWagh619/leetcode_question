9fe4b19550372945b9487a3ae5cca646c87f1f86
RahulWagh619 <rahulwagh196122@gmail.com>
2025-09-26 14:45:59 +0530

    Time: 173 ms (17.59%), Space: 16.5 MB (95.74%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
+        if (nums.size() < 3) {
+            return 0;
+        }
         sort(nums.begin(),nums.end());
         long long ans=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                for(int k=j+1;k<nums.size();k++){
-                    long long val1=nums[i];
-                    long long val2=nums[j];
-                    long long val3=nums[k];
-                    if((val1+val2>val3 && val1+val3>val2) && val3+val2>val1){
-                        ans++;
-                    }
-                }
+        for(int i=0;i<nums.size()-2;i++){
+            for(int j=i+1;j<nums.size()-1;j++){
+                long long sum=nums[i]+nums[j];
+                auto it=lower_bound(nums.begin()+j+1,nums.end(),sum);
+                ans += distance(nums.begin() + j + 1, it);
             }
         }
         return ans;
