5bd817cdf362e9f08775137404deb4fb1983d516
RahulWagh619 <rahulwagh196122@gmail.com>
2025-09-26 22:22:49 +0530

    Time: 155 ms (19.51%), Space: 67.4 MB (9.95%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,45 +1,53 @@
-class Solution {
+class Solution
+{
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int count1=0;
-        int count2=0;
-        map<int,int>mpp;
-        int l=0;
-        int r=0;
-        while(r<nums.size()){
+    int subarraysWithKDistinct(vector<int> &nums, int k)
+    {
+        int count1 = 0;
+        int count2 = 0;
+        map<int, int> mpp;
+        int l = 0;
+        int r = 0;
+        while (r < nums.size())
+        {
             mpp[nums[r]]++;
-            while(mpp.size()>k && l<r){
+            while (mpp.size() > k)
+            {
                 mpp[nums[l]]--;
-                 if (mpp[nums[l]] == 0) {
-        mpp.erase(nums[l]);
-    }
+                if (mpp[nums[l]] == 0)
+                {
+                    mpp.erase(nums[l]);
+                }
 
                 l++;
             }
-            count1+=(r-l+1);
+            count1 += (r - l + 1);
             r++;
         }
-        cout<<count1<<endl;
-        if(k==1){
-            return count1;
-        }
-        // cout<<count1<<endl;
+        // cout << count1 << endl;
         mpp.clear();
-        l=0;
-        r=0;
-        while(r<nums.size()){
+        l = 0;
+        r = 0;
+        while (r < nums.size())
+        {
             mpp[nums[r]]++;
-            while(mpp.size()>k-1 && l<r){
+            while (mpp.size() > k - 1)
+            {
                 mpp[nums[l]]--;
-                 if (mpp[nums[l]] == 0) {
-        mpp.erase(nums[l]);
-    }
+                if (mpp[nums[l]] == 0)
+                {
+                    mpp.erase(nums[l]);
+                }
 
                 l++;
             }
-            count2+=(r-l+1);
+            // if (l <= r)
+            // {
+                count2 += (r - l + 1);
+            // }
             r++;
         }
-        return count1-count2;
+        cout << count2 << endl;
+        return count1 - count2;
     }
 };
