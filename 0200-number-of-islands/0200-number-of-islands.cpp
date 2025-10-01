b045f218fafb9d73bbf20b817f432010c2e74a8d
RahulWagh619 <rahulwagh196122@gmail.com>
2025-10-01 17:55:50 +0530

    Time: 25 ms (72.43%), Space: 17.1 MB (57.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-void bfs(int x,int y,int n,int m,vector<vector<char>>& grid,vector<int>&vis){
-        queue<pair<int,int>>q;
+void dfs(int x,int y,int n,int m,vector<vector<char>>& grid,vector<int>&vis){
         vis[x*m+y]=1;
-        q.push({x,y});
-        while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
-            q.pop();
+        int i=x;
+        int j=y;
               if(i-1>=0 && vis[((i-1)*m)+j]==0 && grid[i-1][j]=='1'){
-                q.push({i-1,j});
-                vis[((i-1)*m)+j]=1;
+               dfs(i-1,j,n,m,grid,vis);
               }
               if(j-1>=0 && vis[(i*m)+j-1]==0 && grid[i][j-1]=='1'){
-                q.push({i,j-1});
-                vis[(i*m)+j-1]=1;
+                 dfs(i,j-1,n,m,grid,vis);
               }
               if(i+1<n && vis[((i+1)*m)+j]==0 && grid[i+1][j]=='1'){
-                q.push({i+1,j});
-                vis[((i+1)*m)+j]=1;
+                 dfs(i+1,j,n,m,grid,vis);
               }
               if(j+1<m && vis[(i*m)+j+1]==0 && grid[i][j+1]=='1'){
-                q.push({i,j+1});
-                vis[(i*m)+j+1]=1;
+                dfs(i,j+1,n,m,grid,vis);
               }
-        }
 }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
@@ -35,7 +26,7 @@ void bfs(int x,int y,int n,int m,vector<vector<char>>& grid,vector<int>&vis){
             for(int j=0;j<m;j++){
                 int index=i*m+j;
                 if(vis[index]==0 && grid[i][j]=='1'){
-                     bfs(i,j,n,m,grid,vis);
+                     dfs(i,j,n,m,grid,vis);
                      count++;
                 }
             }
