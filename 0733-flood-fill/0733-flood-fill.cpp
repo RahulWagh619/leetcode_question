8ad1ef7b1efe5ae97eb18bb699bcbd0ba05b811e
RahulWagh619 <rahulwagh196122@gmail.com>
2025-10-02 23:03:03 +0530

    Time: 0 ms (100%), Space: 18.2 MB (46.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
-void bfs(vector<vector<int>>& image, int sr, int sc, int color,vector<int>&vis){
+void dfs(vector<vector<int>>& image, int sr, int sc, int color,vector<int>&vis,int val){
         int n=image.size();
         int m=image[0].size();
-        int val=image[sr][sc];
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
         int x=sr;
         int y=sc;
         image[x][y]=color;
         vis[(x*m)+y]=1;
-        while(!q.empty()){
-            x=q.front().first;
-            y=q.front().second;
-            q.pop();
             if(x-1>=0 && image[x-1][y]==val && vis[(x-1)*m+y]==0){
-                image[x-1][y]=color;
-                vis[(x-1)*m+y]=1;
-                q.push({x-1,y});
+                dfs(image,x-1,y,color,vis,val);
             }
             if(x+1<n && image[x+1][y]==val && vis[(x+1)*m+y]==0){
-                image[x+1][y]=color;
-                vis[(x+1)*m+y]=1;
-                q.push({x+1,y});
-
+                dfs(image,x+1,y,color,vis,val);
             }
             if(y-1>=0 && image[x][y-1]==val && vis[(x)*m+y-1]==0){
-                image[x][y-1]=color;
-                vis[(x)*m+y-1]=1;
-                q.push({x,y-1});
+                 dfs(image,x,y-1,color,vis,val);
 
             }
             if(y+1<m && image[x][y+1]==val && vis[(x)*m+y+1]==0){
-                image[x][y+1]=color;
-                vis[(x)*m+y+1]=1;
-                q.push({x,y+1});
+                dfs(image,x,y+1,color,vis,val);
             }
-        }
 
 }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
         vector<int>vis((n*m)+1,0);
-        bfs(image,sr,sc,color,vis);
+        int val=image[sr][sc];
+        dfs(image,sr,sc,color,vis,val);
         return image;
     }
 };
