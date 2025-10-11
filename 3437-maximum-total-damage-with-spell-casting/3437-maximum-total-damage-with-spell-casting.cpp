ece554f6040bc7dd795321590e479db20d28b074
RahulWagh619 <rahulwagh196122@gmail.com>
2025-10-11 15:31:55 +0530

    Time: 400 ms (36.05%), Space: 205.5 MB (64.99%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
 typedef long long ll;
-ll dp[100002];
-ll helper(int i,vector<int>& power, unordered_map<ll,ll>&mpp){
-    if(i>=power.size()){
-        return 0;
-    }
-    if(dp[i]!=-1)return dp[i];
-    ll maxi=INT_MIN;
-    ll val=upper_bound(power.begin(),power.end(),power[i]+2)-power.begin();
-    ll pick=helper(val,power,mpp)+power[i]*mpp[power[i]];
-    ll next=upper_bound(power.begin(),power.end(),power[i])-power.begin();
-    ll notpick=helper(next,power,mpp);
-    return dp[i]=max(pick,notpick);
-}
+// ll dp[100002];
+// ll helper(int i,vector<int>& power, unordered_map<ll,ll>&mpp){
+//     if(i>=power.size()){
+//         return 0;
+//     }
+//     if(dp[i]!=-1)return dp[i];
+//     ll maxi=INT_MIN;
+//     ll val=upper_bound(power.begin(),power.end(),power[i]+2)-power.begin();
+//     ll pick=helper(val,power,mpp)+power[i]*mpp[power[i]];
+//     ll notpick=helper(i+1,power,mpp);
+//     return dp[i]=max(pick,notpick);
+// }
     long long maximumTotalDamage(vector<int>& power) {
-        memset(dp,-1,sizeof dp);
+        int n=power.size();
+        vector<ll>dp(n+1,0);
         sort(power.begin(),power.end());
         unordered_map<ll,ll>mpp;
-        int n=power.size();
         for(int i=0;i<n;i++){
             mpp[power[i]]++;
         }
-        return helper(0,power,mpp);
+        for(int i=n-1;i>=0;i--){
+        ll val=upper_bound(power.begin(),power.end(),power[i]+2)-power.begin();
+          ll pick=dp[val]+power[i]*mpp[power[i]];
+          ll next=upper_bound(power.begin(),power.end(),power[i])-power.begin();
+          ll notpick=dp[next];
+          dp[i]=max(pick,notpick);
+        }
+        return dp[0];
     }
 };
