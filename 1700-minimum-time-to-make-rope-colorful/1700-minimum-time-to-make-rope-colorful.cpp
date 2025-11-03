a4a8874316a7425ba0922e9c5866c33737fdadbf
RahulWagh619 <rahulwagh196122@gmail.com>
2025-11-03 20:10:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n = colors.length();
-        int maxi = INT_MIN;
-        int ans = 0;
-        int cost = neededTime[0];
-        
-        for(int i = 1; i < n; i++){
-            cost += neededTime[i];
-            
-            if(colors[i] == colors[i-1]){
-                maxi = max(maxi, max(neededTime[i], neededTime[i-1]));
+        int n=colors.length();
+        int maxi=INT_MIN;
+        int ans=0;
+        int cost=neededTime[0];
+        for(int i=1;i<n;i++){
+            cost+=neededTime[i];
+            if(colors[i]==colors[i-1]){
+                maxi=max(maxi,max(neededTime[i],neededTime[i-1]));
             }
             else{
-                cost -= neededTime[i];
-                if(maxi != INT_MIN){
-                    ans += (cost - maxi);
+                cost-=neededTime[i];
+                if(maxi!=INT_MIN){
+                    ans+=(cost-maxi);
                 }
-                cost = neededTime[i];
-                maxi = INT_MIN;
+                    cost=neededTime[i];
+                maxi=INT_MIN;
             }
+            cout<<i<<" "<<maxi<<" "<<ans<<endl;
         }
-        
-        if(maxi != INT_MIN){
-            ans += (cost - maxi);
+        cout<<cost<<" "<<maxi<<endl;
+        if(maxi!=INT_MIN){
+        ans+=cost-maxi;
         }
-        
         return ans;
     }
 };
