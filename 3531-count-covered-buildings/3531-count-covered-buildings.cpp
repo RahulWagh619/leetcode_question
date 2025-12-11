fcf705fa83fac5cafd0df6c1c998531f6baecb28
RahulWagh619 <rahulwagh196122@gmail.com>
2025-12-11 15:32:01 +0530

    Time: 924 ms (21.55%), Space: 394.4 MB (43.09%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        int count = 0;
-        map<int, vector<int>> mppx;  // x -> list of y values
-        map<int, vector<int>> mppy;  // y -> list of x values
-        
-        // Populate maps
-        for (auto& building : buildings) {
-            int x = building[0];
-            int y = building[1];
+        int count=0;
+        map<int,vector<int>>mppx;
+        map<int,vector<int>>mppy;
+        int size=buildings.size();
+        for(int i=0;i<size;i++){
+            int x=buildings[i][0];
+            int y=buildings[i][1];
             mppx[x].push_back(y);
             mppy[y].push_back(x);
         }
-        
-        // Sort all vectors
-        for (auto& pair : mppx) {
-            sort(pair.second.begin(), pair.second.end());
+        for(auto &i:mppx){
+            sort(i.second.begin(),i.second.end());
         }
-        for (auto& pair : mppy) {
-            sort(pair.second.begin(), pair.second.end());
+        for(auto &i:mppy){
+            sort(i.second.begin(),i.second.end());
         }
-        
-        // Check each building
-        for (auto& building : buildings) {
-            int x = building[0];
-            int y = building[1];
-            vector<int>& ysAtX = mppx[x];
-            vector<int>& xsAtY = mppy[y];
-            
-            // Check if building has neighbors on all 4 sides
-            bool hasYBelow = ysAtX[0] < y;
-            bool hasYAbove = ysAtX[ysAtX.size() - 1] > y;
-            bool hasXLeft = xsAtY[0] < x;
-            bool hasXRight = xsAtY[xsAtY.size() - 1] > x;
-            
-            if (hasYBelow && hasYAbove && hasXLeft && hasXRight) {
+        for(int i=0;i<size;i++){
+            int x=buildings[i][0];
+            int y=buildings[i][1];
+            if(mppx[x].size()>2 && mppy[y].size()>2){
+                if(mppx[x][0]<y&& mppx[x][mppx[x].size()-1]>y){
+                    if(mppy[y][0]<x && mppy[y][mppy[y].size()-1]>x){
+
                 count++;
+                    }
+
+                }
             }
         }
-        
         return count;
     }
 };
