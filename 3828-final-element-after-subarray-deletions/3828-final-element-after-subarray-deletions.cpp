ba1a91f7c08d3664287e54181dbb2ab507ef2889
RahulWagh619 <rahulwagh196122@gmail.com>
2026-02-07 23:45:12 +0530

    Time: 4 ms (5.74%), Space: 179.8 MB (83.42%) - LeetHub

---
+++ 3828-final-element-after-subarray-deletions/3828-final-element-after-subarray-deletions.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        int val=max(nums[0],nums[n-1]);
+        return val;
+
+    }
+};
